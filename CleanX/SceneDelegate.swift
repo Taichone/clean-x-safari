c4aa98a5d930869ecc5730a1dd9ff50a2ed53333
Taichi Miki <taichone.dev@gmail.com>
2026-05-09 15:38:57 +0900

    Initial Commit

---
+++ CleanX/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  CleanX
+//
+//  Created by Taichone on 2026/05/09.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
